0c5fe9db6b06569bbdb70868cded4e211dd03d38
Dinesh Kumar <demo170194@gmail.com>
2026-05-29 20:03:52 +0530

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT');
+
+  if (!port) {
+    throw new Error('PORT environment variable is not defined');
+  }
+
+  await app.listen(port);
+
+  console.log(`Application started successfully on -- ${port}` );
+}
+
+bootstrap();
